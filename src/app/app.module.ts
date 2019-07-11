5f3f94fd1d2dca37d553f91d0681af1f3aeb65fc
ugur <ugurcancakar@hotmail.com>
2019-07-11 18:01:47 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      ProductAlertsComponent,
+      ProductDetailsComponent,
+      ProductListComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule,
+      FormsModule,
+      RouterModule.forRoot([
+         { path: '', component: ProductListComponent },
+         { path: 'products/:productId', component: ProductDetailsComponent },
+       ])
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
